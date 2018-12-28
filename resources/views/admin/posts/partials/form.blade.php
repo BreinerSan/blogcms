487e536fde1096a875ddb1d12fe9ad3a9a762d7c
Breiner <breiner.sv@gmail.com>
2018-12-27 23:19:31 -0500

    Adicion del ckeditor

--- resources/views/admin/posts/partials/form.blade.php
+++ resources/views/admin/posts/partials/form.blade.php
@@ -57,6 +57,7 @@
 
 @section('scripts')
 	<script src="{{ asset('vendor/StringToSlug/jquery.stringToSlug.min.js') }}"></script>
+	<script src="{{ asset('vendor/ckeditor/ckeditor.js') }}"></script>
 	<script>
 		$(document).ready(function(){
 			$('#name, #slug').stringToSlug({
@@ -65,5 +66,11 @@
 				}
 			});
 		});
+
+		CKEDITOR.config.height = 400;
+		CKEDITOR.config.width = 'auto';
+
+		CKEDITOR.replace('body');
+
 	</script>
 @endsection
